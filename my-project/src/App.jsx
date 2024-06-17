69af80c59943bd6ac22c8ff4027f45769974a2e5
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-06-17 23:09:40 +0530

    Update App.jsx

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -1,1 +1,28 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  
+  return (
+    <>
+      <div className='container'>
+        <div>
+          <label htmlFor="name">Name : </label>
+          <input type="text" />
+        </div>
+        <div>
+          <label htmlFor="">Phone :</label>
+          <input type="number" name="number" />
+        </div>
+        <button className='btn btn-primary'>Submit</button>
+      </div>
+    </>
+  )
+}
+
+export default App
 
