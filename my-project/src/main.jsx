6f151b5436a3f9dc6983b11c9f39e2f191cac46c
Abhinav U <129955044+abhinavhh@users.noreply.github.com>
2024-06-24 22:05:01 +0530

    Update main.jsx

--- my-project/src/main.jsx
+++ my-project/src/main.jsx
@@ -4,11 +4,21 @@ import App from './App.jsx'
 import './index.css'
 import Main from './State.jsx'
 import CondRend from './ConditionalRender.jsx'
+import Load from './forLoading.jsx'
+import Clicks from './ClickEvent.jsx'
+import Counter from './hooks/CounterApp.jsx'
+import Counter2 from './hooks/useeffect.jsx'
+import News from './newsApp/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <Main/> */}
-    <CondRend/>
+    {/* <CondRend/> */}
+    {/* <Load/> */}
+    {/* <Clicks /> */}
+    {/* <Counter /> */}
+    {/* <Counter2 /> */}
+    <News />
   </React.StrictMode>,
 )
